1a40092266cf30ffc884fd1ebbcdd6cb7a49f810
Lukas Diekmann <mail@lukas-diekmann.de>
2025-10-18 16:59:08 +0200

    dynamic api url

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types.js';
 import type { Drill } from '$lib/types/Drill.js';
 
 export const load = (async () => {
-    const response = await fetch(`http://127.0.0.1:8000/api/drills`);
+    const response = await fetch(`${import.meta.env.PUBLIC_API_URL}/api/drills`);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
--- frontend/src/routes/[id]/+page.ts
+++ frontend/src/routes/[id]/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from './$types.js';
 import type { Drill } from '$lib/types/Drill.js';
 
 export const load = (async ({ params }: { params: { id: string } }) => {
-    const response = await fetch(`http://127.0.0.1:8000/api/drills/${params.id}`);
+    const response = await fetch(`${import.meta.env.PUBLIC_API_URL}/api/drills/${params.id}`);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
